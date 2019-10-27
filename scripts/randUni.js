751f181d8cf1118c35e2af1b1cef4bced3a96008
Simeon Simeonov <simsimnvv@gmail.com>
2019-10-28 00:02:32 +0200

    ui fixes

--- scripts/randUni.js
+++ scripts/randUni.js
@@ -1,49 +1,49 @@
-function startApp(){
+function startApp() {
+    let verbs = ['ближат', 'пиянстват', 'мързелуват', 'дъртеят', 'зяпат', 'тъпеят'];
+    let action = ['пиене', 'спане', 'мързелуване'];
+    let noun = ['бира', 'отлежала ракия', 'мързел'];
+    let nouns = ['изхабени бъбреци', 'анаболи', 'отлежала ракия', 'простащина'];
+    let adjective = ['селски', 'неадекватен', 'бавен', 'бъгав', 'секси', 'счупен', 'тъп', 'просташки', 'глупав', 'безполезен'];
+    let adjectives = ['мързеливи', 'заспали', 'възбудени', 'криминални', 'скандални', 'миризливи', 'глупави', 'неадекватни', 'безполезни'];
+    let people = ['майка ти', 'баба ти', 'стринка ти'];
 
-    let verbs=['ближат','пиянстват','мързелуват','дъртеят','зяпат','тъпеят'];
-    let action=['пиене','спане','мързелуване'];
-    let noun=['бира','отлежала ракия','мързел'];
-    let nouns=['изхабени бъбреци','анаболи','отлежала ракия','простащина'];
-    let adjective=['селски','неадекватен','бавен','бъгав','секси','счупен','тъп','просташки','глупав','безполезен'];
-    let adjectives=['мързеливи','заспали', 'възбудени','криминални','скандални','миризливи','глупави','неадекватни','безполезни'];
-    let people=['майка ти', 'баба ти', 'стринка ти'];
-
-    function getRandomWord(wordsArray){
+    function getRandomWord(wordsArray) {
         let randNum= Math.floor(Math.random() * wordsArray.length);
         return wordsArray[randNum];
     }
 
-    function randomizeWords(){
+    function randomizeWords() {
         let words=$('.random-word');
-        for(let word of words){
-            let className= $(word).attr('class').split(' ')[1];
-            let randomWord='';
-            switch (className){
+        for(let word of words) {
+            let className = $(word).attr('class').split(' ')[1];
+            let randomWord = '';
+            switch(className) {
                 case 'noun':
-                    randomWord=getRandomWord(noun);
+                    randomWord = getRandomWord(noun);
                     break;
                 case 'nouns':
-                    randomWord=getRandomWord(nouns);
+                    randomWord = getRandomWord(nouns);
                     break;
                 case 'action':
-                    randomWord=getRandomWord(action);
+                    randomWord = getRandomWord(action);
                     break;
                 case 'adjectives':
-                    randomWord=getRandomWord(adjectives);
+                    randomWord = getRandomWord(adjectives);
                     break;
                 case 'adjective':
-                    randomWord=getRandomWord(adjective);
+                    randomWord = getRandomWord(adjective);
                     break;
                 case 'verbs':
-                    randomWord=getRandomWord(verbs);
+                    randomWord = getRandomWord(verbs);
                     break;
                 case 'people':
-                    randomWord=getRandomWord(people);
+                    randomWord = getRandomWord(people);
                     break;
             }
             $(word).text(randomWord);
         }
     }
+
     $('.randomize-btn').on('click',randomizeWords);
     randomizeWords();
 }
